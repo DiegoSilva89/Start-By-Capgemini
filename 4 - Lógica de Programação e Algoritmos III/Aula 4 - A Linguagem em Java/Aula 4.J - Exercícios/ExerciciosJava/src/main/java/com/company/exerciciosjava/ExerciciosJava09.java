71ebe783f4c390f9442e86f7fd992f88dc748235
Diego Silva <diegosantosdasilva89@gmail.com>
2023-01-24 21:30:04 -0300

    Aula 4J - Exercício 09

---
+++ 4 - Lógica de Programação e Algoritmos III/Aula 4 - A Linguagem em Java/Aula 4.J - Exercícios/ExerciciosJava/src/main/java/com/company/exerciciosjava/ExerciciosJava09.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.company.exerciciosjava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Diego Silva
+ */
+public class ExerciciosJava09 {
+    
+    public static void main(String[] args) {
+        
+        //======================================================================
+            /*Faça um algoritmo que receba um valor que foi depositado e exiba o
+            valor com rendimento após um mês.Considere fixo o juro da poupança 
+            em 0,07% a. m;*/
+        //======================================================================
+        
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Qual o valor depositado na Poupança? ");
+        double deposito = leitor.nextDouble();
+        
+        double valorMes = deposito * 1.007;
+        
+        System.out.printf("Após 1 mês na poupança, o valor será: %.2f", valorMes);
+                               
+    }
+    
+}
