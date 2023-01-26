df25cbffbef9b6ebf1eff1c4f3d6ef41a15c6e58
Diego Silva <diegosantosdasilva89@gmail.com>
2023-01-26 05:10:52 -0300

    Aula 4N

--- 4 - Lógica de Programação e Algoritmos III/Aula 4 - A Linguagem em Java/Aula 4.N - Exercício Estrutura Repetição/ExercicioEstruturaRepeticao/src/main/java/com/company/exercicioestruturarepeticao/ExercicioEstruturaRepeticao.java
+++ 4 - Lógica de Programação e Algoritmos III/Aula 4 - A Linguagem em Java/Aula 4.N - Exercício Estrutura Repetição/ExercicioEstruturaRepeticao/src/main/java/com/company/exercicioestruturarepeticao/ExercicioEstruturaRepeticao.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.company.estruturawhile;
+package com.company.exercicioestruturarepeticao;
 
 /**
  *
  * @author diego
  */
-public class EstruturaWhile {
+public class ExercicioEstruturaRepeticao {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
