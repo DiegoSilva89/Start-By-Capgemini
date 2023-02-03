5b8b7b9166cadef64b4e886c5cd9c87f2a6d68bf
DiegoSilva89 <diegosantosdasilva89@gmail.com>
2023-02-03 03:54:04 -0300

    Aula 7H - Exercício Interface Gráfica

---
+++ 4 - Lógica de Programação e Algoritmos III/Aula 7 - Interfaces Gráficas/Aula 7H - Exercício Interface Gráfica/ExercicioInterfaceGrafica/src/main/java/com/mycompany/exerciciointerfacegrafica/ExercicioInterfaceGrafica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exerciciointerfacegrafica;
+
+/**
+ *
+ * @author diego
+ */
+public class ExercicioInterfaceGrafica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
