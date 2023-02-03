8e2c3aded8425c7a2639337c2b7ecfcc4149ae2c
DiegoSilva89 <diegosantosdasilva89@gmail.com>
2023-02-03 02:02:34 -0300

    Aula 7G - Minha Interface

---
+++ 4 - Lógica de Programação e Algoritmos III/Aula 7 - Interfaces Gráficas/Aula 7G - Minha interface gráfica/MinhaInterfaceGrafica/src/main/java/com/mycompany/minhainterfacegrafica/MinhaInterfaceGrafica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.minhainterfacegrafica;
+
+/**
+ *
+ * @author Diego Silva
+ */
+public class MinhaInterfaceGrafica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
